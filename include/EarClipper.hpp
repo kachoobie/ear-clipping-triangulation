e31214ee2c709ce400e4e6586da9d6c267a007d6
kachoobie <realkachoobie@gmail.com>
2024-03-30 22:43:45 -1000

    Implement getAngle method

--- include/EarClipper.hpp
+++ include/EarClipper.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cmath>
 
 #include "Point2D.hpp"
 
--- src/EarClipper.cpp
+++ src/EarClipper.cpp
@@ -16,5 +16,12 @@ void EarClipper::triangulate()
 
 float EarClipper::getAngle(Point2D shared, Point2D p1, Point2D p2)
 {
-    return 1.1f;
+    float m1, m2;
+
+    m1 = (p1._y - shared._y) / (p1._x - shared._x);
+    m2 = (p2._y - shared._y) / (p2._x - shared._x);
+
+    float theta = std::atan((m1 - m2) / (1 + m1 * m2)) * 180 / M_PI;
+
+    return theta;
 }
